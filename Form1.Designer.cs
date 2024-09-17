f3c4e9414eca50ae30fbc0023034d25c70e88b0f
mvishok <vishokmanikantan@gmail.com>
2024-09-17 20:34:36 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Shoot_Scoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.status1 = new System.Windows.Forms.Label();
+            this.status2 = new System.Windows.Forms.Label();
+            this.shortcut = new System.Windows.Forms.Label();
+            this.vishok = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Shoot_Scoot.Properties.Resources.ss;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // status1
+            // 
+            this.status1.AutoSize = true;
+            this.status1.BackColor = System.Drawing.Color.White;
+            this.status1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status1.Location = new System.Drawing.Point(29, 249);
+            this.status1.Name = "status1";
+            this.status1.Size = new System.Drawing.Size(154, 20);
+            this.status1.TabIndex = 1;
+            this.status1.Text = "Screenshot mode is:";
+            // 
+            // status2
+            // 
+            this.status2.AutoSize = true;
+            this.status2.BackColor = System.Drawing.Color.White;
+            this.status2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status2.ForeColor = System.Drawing.Color.Green;
+            this.status2.Location = new System.Drawing.Point(186, 249);
+            this.status2.Name = "status2";
+            this.status2.Size = new System.Drawing.Size(73, 20);
+            this.status2.TabIndex = 2;
+            this.status2.Text = "ACTIVE";
+            // 
+            // shortcut
+            // 
+            this.shortcut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.shortcut.AutoSize = true;
+            this.shortcut.BackColor = System.Drawing.Color.White;
+            this.shortcut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shortcut.Location = new System.Drawing.Point(3, 10);
+            this.shortcut.Name = "shortcut";
+            this.shortcut.Size = new System.Drawing.Size(294, 20);
+            this.shortcut.TabIndex = 4;
+            this.shortcut.Text = "Press ALT + INSERT to take screenshot";
+            // 
+            // vishok
+            // 
+            this.vishok.AutoSize = true;
+            this.vishok.BackColor = System.Drawing.Color.White;
+            this.vishok.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vishok.Location = new System.Drawing.Point(46, 282);
+            this.vishok.Name = "vishok";
+            this.vishok.Size = new System.Drawing.Size(209, 16);
+            this.vishok.TabIndex = 6;
+            this.vishok.Text = "Developed by Vishok Manikantan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.vishok);
+            this.Controls.Add(this.shortcut);
+            this.Controls.Add(this.status2);
+            this.Controls.Add(this.status1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_LoadAsync);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label status1;
+        private System.Windows.Forms.Label status2;
+        private System.Windows.Forms.Label shortcut;
+        private System.Windows.Forms.Label vishok;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace Shoot_Scoot
+{
+    public partial class Form1 : Form
+    {
+        string PATH = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        int count = 0;
+        const uint MOD_ALT = 0x0001; // Alt key
+        const uint VK_S = 0x53;      // S key
+        const uint VK_INSERT = 0x2D; // Insert key
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int MYACTION_HOTKEY_ID = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+
+            //PREVENT MULTIPLE INSTANCES
+            if (System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("Another instance of the application is already running", "Shoot&Scoot", MessageBoxButtons.OK, MessageBoxIcon.Information);   
+                Application.Exit();
+            }
+
+            if (!Directory.Exists(PATH))
+            {
+                Directory.CreateDirectory(PATH);
+            }
+
+            await Task.Delay(1200);
+
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID, MOD_ALT, VK_INSERT);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID + 1, MOD_ALT, VK_S);
+        }
+
+        // Handle the hotkeys
+        protected override void WndProc(ref Message m)
+        {
+            
+            const int WM_HOTKEY = 0x0312;
+            if (m.Msg == WM_HOTKEY)
+            {
+                int id = m.WParam.ToInt32();
+                if (id == MYACTION_HOTKEY_ID) // alt + insert
+                {
+
+                    TakeScreenshot();
+
+                    //change pictureBox dimensions
+                    (int width, int height) = GetResizedDimensions(Path.Combine(PATH, count - 1 + ".png"), 300, 300);
+                    pictureBox1.Size = new Size(width, height);
+                    //resize form
+                    this.Size = new Size(width, height);
+
+                    pictureBox1.Image = Image.FromFile(Path.Combine(PATH, count - 1 + ".png"));
+                    shortcut.Hide();
+                    vishok.Hide();
+                    status1.Hide();
+                    status2.Hide();
+
+                    //show form
+                    this.WindowState = FormWindowState.Normal;
+                    this.ShowInTaskbar = true;
+                    this.Show();
+                    this.Activate();
+                    this.BringToFront();
+
+                    Task.Delay(500).Wait();
+                    
+                    //release and dispose image
+                    pictureBox1.Image.Dispose();
+                    pictureBox1.Image = null;
+
+                    this.WindowState = FormWindowState.Minimized;
+                    this.ShowInTaskbar = false;
+                    this.Hide();
+
+                    RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID, MOD_ALT, VK_INSERT);
+                    RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID + 1, MOD_ALT, VK_S);
+                } 
+
+                else if (id  == MYACTION_HOTKEY_ID + 1)
+                {
+                    if (count == 0)
+                    {
+                        pictureBox1.Size = new Size(300, 300);
+                        this.Size = new Size(300, 300);
+                        pictureBox1.Image = Shoot_Scoot.Properties.Resources.ss;
+                        shortcut.Show();
+                        vishok.Show();
+                        status1.Show();
+                        status2.Show();
+
+                        status2.Text = "INACTIVE";
+                        status2.ForeColor = Color.Red;
+
+                        shortcut.Text = "Exiting Shoot&Scoot";
+
+                        Task.Delay(800).Wait();
+                        Directory.Delete(PATH, true);
+                        Application.Exit();
+                    }
+
+                    string pdfPath = CompilePDF();
+                    if (pdfPath != null)
+                    {
+                        pictureBox1.Size = new Size(300, 300);
+                        this.Size = new Size(300, 300);
+                        pictureBox1.Image = Shoot_Scoot.Properties.Resources.ss;
+                        shortcut.Show();
+                        vishok.Show();
+                        status1.Show();
+                        status2.Show();
+
+                        status2.Text = "INACTIVE";
+                        status2.ForeColor = Color.Red;
+
+                        shortcut.Text = "Thank you for using Shoot&Scoot!";
+
+                        //show
+                        this.WindowState = FormWindowState.Normal;
+                        this.ShowInTaskbar = true;
+                        this.Show();
+                        this.Activate();
+                        this.BringToFront();
+
+                        Task.Delay(1500).Wait();
+
+                        Directory.Delete(PATH, true);
+
+                        Application.Exit();
+                    }
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        // Unregister the hotkeys when closing the form
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID);
+            UnregisterHotKey(this.Handle, MYACTION_HOTKEY_ID + 1);
+        }
+
+        private void TakeScreenshot()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                string filename = Path.Combine(PATH, count + ".png");
+                try
+                {
+                    bitmap.Save(filename, System.Drawing.Imaging.ImageFormat.Png);
+                    count++;
+                } catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+
+        }
+
+        private string CompilePDF()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files|*.pdf";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (PdfDocument doc = new PdfDocument())
+                {
+                    foreach (var file in Directory.GetFiles(PATH, "*.png"))
+                    {
+                        PdfPage page = doc.AddPage();
+                        XGraphics gfx = XGraphics.FromPdfPage(page);
+                        XImage image = XImage.FromFile(file);
+
+                        //aspect ratio of the image
+                        double imageAspectRatio = (double)image.PixelWidth / image.PixelHeight;
+                        double pageAspectRatio = (double)page.Width.Point / page.Height.Point; // Use .Point
+
+                        double newWidth, newHeight;
+
+                        if (imageAspectRatio > pageAspectRatio)
+                        {
+                            newWidth = page.Width.Point; // Use .Point
+                            newHeight = newWidth / imageAspectRatio;
+                        }
+                        else
+                        {
+                            newHeight = page.Height.Point; // Use .Point
+                            newWidth = newHeight * imageAspectRatio;
+                        }
+
+                        //to center the image on the page
+                        double xOffset = (page.Width.Point - newWidth) / 2; // Use .Point
+                        double yOffset = (page.Height.Point - newHeight) / 2; // Use .Point
+
+                        gfx.DrawImage(image, xOffset, yOffset, newWidth, newHeight);
+                    }
+                    doc.Save(saveFileDialog.FileName);
+                    return saveFileDialog.FileName;
+                }
+            }
+            return null;
+        }
+
+        public static (int width, int height) GetResizedDimensions(string imagePath, int maxWidth, int maxHeight)
+        {
+            using (Image image = Image.FromFile(imagePath))
+            {
+                float imageAspectRatio = (float)image.Width / image.Height;
+
+                int width = maxWidth;
+                int height = (int)(width / imageAspectRatio);
+
+                if (height > maxHeight)
+                {
+                    height = maxHeight;
+                    width = (int)(height * imageAspectRatio);
+                }
+
+                return (width, height);
+            }
+        }
+    }
+}
